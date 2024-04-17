ad1a5a4a1edcf3c823f1330cd73228614cda36a5
majjeed <abdulmajeed.hersi@gmail.com>
2024-04-17 16:30:03 +0100

    complete task 6 & 7 except for divide by zero

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ const multiply = (a, b) => {
 };
 
 const divide = (a, b) => {
+    // if (b === 0) {
+    //     console.log("Don't try to divide by zero!");
+    //     calcDisplay.textContent = '';
+    //     return 
+    // };
     return a / b;
 };
 
@@ -25,6 +30,8 @@ let secondNum = 0;
 let operator = 'none';
 
 const operate = (firstNum, secondNum, operator) => {
+    firstNum = +(parseFloat(firstNum).toFixed(2));
+    secondNum = +(parseFloat(secondNum).toFixed(2));
     let result = 0;
     switch(operator){
         case '+':
@@ -57,18 +64,6 @@ const optBtns = document.querySelectorAll('.optBtn');
 const calcDisplay = document.querySelector('.calcDisplay');
 let calcDisplayVal = calcDisplay.textContent;
 
-// numBtns.forEach((btn) => {
-//     btn.addEventListener('click', () => {
-//         calcDisplay.textContent += btn.textContent;
-//     })
-// });
-
-// optBtns.forEach((btn) => {
-//     btn.addEventListener('click', () => {
-//         calcDisplay.textContent += btn.textContent;
-//     })
-// });
-
 const addToDisplay = (btns) => {
     btns.forEach((btn) => {
         btn.addEventListener('click', () => {
@@ -80,4 +75,22 @@ const addToDisplay = (btns) => {
 
 addToDisplay(numBtns);
 addToDisplay(optBtns);
+
+const clearBtn = document.querySelector('.clearBtn');
+clearBtn.addEventListener('click', () => {
+    calcDisplay.textContent = '';
+    calcDisplayVal = '';
+});
+
+const equalBtn = document.querySelector('.equalBtn');
+equalBtn.addEventListener('click', () => {
+    //regex for not digit
+    //bug picks up dot instead TODO: ignore dot
+    let optArr = calcDisplay.textContent.match(/\D/g);
+    let numArr = calcDisplay.textContent.split(/\D/);
+    //the order is firstNum, secondNum, operator 
+    let result = operate(numArr[0], numArr[1], optArr[0]).toFixed(2);
+    calcDisplay.textContent = `${result}${optArr[1] || ''}`;
+    calcDisplayVal = calcDisplay.textContent;
+});
 
